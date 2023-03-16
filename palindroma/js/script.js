3368b3f7819c3e3249c5182e77ae570dfaa343b8
StefanoM98 <smartino030@gmail.com>
2023-03-16 14:03:41 +0100

    Add: Project scaffolding

---
+++ palindroma/js/script.js
@@ -0,0 +1,6 @@
+// CONSEGNA
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+const userWordChoise = prompt("Inserisci una parola e posso vedere se questa parola è palindroma oppure no :)")
+
