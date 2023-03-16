7cf79817c7b825d5982030717b2bc8ab6f27d875
StefanoM98 <smartino030@gmail.com>
2023-03-16 14:55:56 +0100

    Add: function isPali

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,5 +2,23 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma.
 
-const userWordChoise = prompt("Inserisci una parola e posso vedere se questa parola è palindroma oppure no :)")
+const userWordChoise = prompt("Inserisci una parola e posso vedere se questa parola è palindroma oppure no :)");
+
+// ORA CREIAMO LA FUNZIONE
+/**
+ * Description
+ * @param {string} userWord
+ * @returns {true || false}
+ */
+
+function isPali(userWord) {
+    const reverseWord = userWord.split('').reverse().join('');
+    return reverseWord === userWord;
+};
+
+if (isPali(userWordChoise)) {
+    console.log(true)
+} else {
+    console.log(false);
+}
 
